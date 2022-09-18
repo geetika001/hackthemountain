bcd23cc60fd69b2445b2ca26aae560a065e706b7
Ektaroy <97738792+Ektaroy@users.noreply.github.com>
2022-09-18 14:30:53 +0530

    new

--- HELPIFYY/js/Index.js
+++ HELPIFYY/js/Index.js
@@ -1,8 +1,8 @@
-let navbar = document.querySelector('.header .navbar');
+// let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
-}
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.add('active');
+// }
 
 document.querySelector('#nav-close').onclick = () =>{
     navbar.classList.remove('active');
